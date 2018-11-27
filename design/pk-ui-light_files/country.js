a4b394c2fd66b488c2c13e6594df6a0f2daeb2d0
Ray <loveyou520babytw@gmail.com>
2018-11-27 12:38:52 +0800

    新增設計稿檔案
    來源六角學院

---
+++ design/pk-ui-light_files/country.js
@@ -0,0 +1,12 @@
+(function () {
+	var country = {'code':'tw','id':44,'defaultLangauge':'zh-tw'};
+	
+	if (!window.evidon) window.evidon = {};
+	
+	if (window.evidon.notice) {
+		window.evidon.notice.setLocation(country);
+	}
+	else {
+		window.evidon.location = country;
+	}
+})();
