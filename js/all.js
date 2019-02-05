e3467b58551fbe14048f2a3d6909facd59a8423c
Ray <loveyou520babytw@gmail.com>
2019-02-05 21:18:23 +0800

    整理JS多餘空格

--- js/all.js
+++ js/all.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    $('#slider').nivoSlider({
-        effect: 'fade',
-        pauseTime: 3000,
-    });
-    $('.btn-e-invoice').click(function (event) { 
-        event.preventDefault();
-        $('.invoice-form').toggleClass('d-none');
-        $('.invoice-form-addr').toggleClass('d-none');
-        $('.btn-e-invoice').toggleClass('active');
-        $('.btn-invoice-addr').toggleClass('active');
-    });
-    $('.btn-invoice-addr').click(function (event) { 
-        event.preventDefault();
-        $('.invoice-form').toggleClass('d-none');
-        $('.invoice-form-addr').toggleClass('d-none');
-        $('.btn-e-invoice').toggleClass('active');
-        $('.btn-invoice-addr').toggleClass('active');
-    });
+  $('#slider').nivoSlider({
+    effect: 'fade',
+    pauseTime: 3000,
+  });
+  $('.btn-e-invoice').click(function (event) {
+    event.preventDefault();
+    $('.invoice-form').toggleClass('d-none');
+    $('.invoice-form-addr').toggleClass('d-none');
+    $('.btn-e-invoice').toggleClass('active');
+    $('.btn-invoice-addr').toggleClass('active');
+  });
+  $('.btn-invoice-addr').click(function (event) {
+    event.preventDefault();
+    $('.invoice-form').toggleClass('d-none');
+    $('.invoice-form-addr').toggleClass('d-none');
+    $('.btn-e-invoice').toggleClass('active');
+    $('.btn-invoice-addr').toggleClass('active');
+  });
 
-    $('.btn-main-sm').click(function (e) { 
-        e.preventDefault();
-        $('.sweet-main-sm').slideToggle();;
-    });
+  $('.btn-main-sm').click(function (e) {
+    e.preventDefault();
+    $('.sweet-main-sm').slideToggle();;
+  });
 });
